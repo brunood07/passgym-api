309123419fe24957c95aa18a945e0587b21bc17b
brunoodlighthouse <bruno.domingues@lighthouseit.com.br>
2023-08-17 20:34:56 -0300

    feat: refresh token generated

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,13 +3,17 @@ import Register from './register'
 import AuthenticateController from './authenticate'
 import ProfileController from './profile'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import RefreshController from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   const register = new Register()
   const authenticate = new AuthenticateController()
+  const refresh = new RefreshController()
 
   app.post('/users', register.handle)
   app.post('/sessions', authenticate.handle)
+
+  app.patch('/token/refresh', refresh.handle)
 
   const profile = new ProfileController()
 
